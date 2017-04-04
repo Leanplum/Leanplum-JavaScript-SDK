0f580bff31c1de3aa215fae26ddfa254763b10ec
Ben Marten <ben.marten@me.com>
2017-04-04 14:08:14 -0700

    Added mocha to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function(grunt) {
-  'use strict';
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
   grunt.initConfig({
     eslint: {
       target: ['lib/leanplum.js'],
@@ -14,21 +18,30 @@ module.exports = function(grunt) {
         },
       },
     },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          quiet: false,
+          clearRequireCache: false,
+          noFail: false,
+          require: ['mock-local-storage', 'test/specs/helpers/setup.js'],
+        },
+        src: ['test/**/*.js'],
+      },
+    },
     watch: {
       js: {
         files: [
           'lib/leanplum.js',
         ],
-        tasks: ['eslint', 'uglify'],
+        tasks: ['lint', 'build', 'test'],
       },
     },
   });
 
-  grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.registerTask('lint', ['eslint']);
   grunt.registerTask('build', ['uglify']);
+  grunt.registerTask('test', ['mochaTest']);
   grunt.registerTask('default', ['watch']);
 };
