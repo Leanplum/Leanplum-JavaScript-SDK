50e50557ba44dc77c3c4a94e463e0d3a0a29b169
Alexis Oyama <alexis@leanplum.com>
2017-08-01 17:06:50 -0700

    refactor(varcache): In the middle of refactoring varcache

--- src/VarCache.js
+++ src/VarCache.js
@@ -16,3 +16,162 @@
  *
  */
 
+import Constants from './Constants'
+import isEqual from 'lodash/isEqual'
+import LocalStorageManager from './LocalStorageManager'
+
+export default class VarCache {
+  static diffs = undefined
+  static variables = undefined
+  static variants = []
+  static hasReceivedDiffs = false
+  static merged = undefined
+  static onUpdate = undefined
+  static token = ''
+  static actionMetadata = {}
+
+  static applyDiffs(diffs, variants, actionMetadata) {
+    VarCache.diffs = diffs
+    VarCache.variants = variants
+    VarCache.actionMetadata = actionMetadata
+    VarCache.hasReceivedDiffs = true
+    VarCache.merged = VarCache.mergeHelper(VarCache.variables, diffs)
+    VarCache.saveDiffs()
+    if (onUpdate) {
+      onUpdate()
+    }
+  }
+
+  static loadDiffs() {
+    try {
+      Leanplum.applyDiffs(
+          JSON.parse(LocalStorageManager.getFromLocalStorage(
+              Constants.DEFAULT_KEYS.VARIABLES) || null),
+          JSON.parse(LocalStorageManager.getFromLocalStorage(
+              Constants.DEFAULT_KEYS.VARIANTS) || null),
+          JSON.parse(LocalStorageManager.getFromLocalStorage(
+              Constants.DEFAULT_KEYS.ACTION_METADATA) || null))
+      VarCache.token = LocalStorageManager.getFromLocalStorage(Constants.DEFAULT_KEYS.TOKEN)
+    } catch (e) {
+      console.log(`Leanplum: Invalid diffs: ${e}`)
+    }
+  }
+
+  static saveDiffs() {
+    LocalStorageManager.saveToLocalStorage(
+        Constants.DEFAULT_KEYS.VARIABLES, JSON.stringify(VarCache.diffs || {}))
+    LocalStorageManager.saveToLocalStorage(
+        Constants.DEFAULT_KEYS.VARIANTS, JSON.stringify(_variants || [])
+    )
+    LocalStorageManager.saveToLocalStorage(Constants.DEFAULT_KEYS.ACTION_METADATA,
+        JSON.stringify(_actionMetadata || {}))
+    LocalStorageManager.saveToLocalStorage(Constants.DEFAULT_KEYS.TOKEN, _token)
+  }
+
+  static setVariables(variables) {
+    _variables = variables
+  }
+
+  static getVariables() {
+    return _merged !== undefined ? _merged : _variables
+  }
+
+  static sendVariables() {
+    let body = {}
+    body[Constants.PARAMS.VARIABLES] = Leanplum._variables
+    Leanplum._request(Constants.METHODS.SET_VARS,
+        new ArgsBuilder().body(JSON.stringify(body)), {
+          sendNow: true
+        })
+  }
+
+  static mergeHelper(vars, diff) {
+    if (typeof diff === 'number' || typeof diff === 'boolean' || typeof diff === 'string') {
+      return diff
+    }
+    if (diff === null || diff === undefined) {
+      return vars
+    }
+
+    let objIterator = function(obj) {
+      return function(f) {
+        if (obj instanceof Array) {
+          for (let i = 0; i < obj.length; i++) {
+            f(obj[i])
+          }
+        } else {
+          for (let attr in obj) {
+            // This seems to be best practice: https://github.com/eslint/eslint/issues/7071
+            // eslint-disable-next-line prefer-reflect
+            if ({}.hasOwnProperty.call(obj, attr)) {
+              f(attr)
+            }
+          }
+        }
+      }
+    }
+    let varsIterator = objIterator(vars)
+    let diffIterator = objIterator(diff)
+
+    // Infer that the diffs is an array if the vars value doesn't exist to tell us the type.
+    let isArray = false
+    if (vars === null) {
+      if (!(diff instanceof Array)) {
+        isArray = null
+        for (let attribute in diff) {
+          if (!diff.hasOwnProperty(attribute)) {
+            continue
+          }
+          if (isArray === null) {
+            isArray = true
+          }
+          if (!(typeof attribute === 'string')) {
+            isArray = false
+            break
+          }
+          if (attribute.length < 3 || attribute.charAt(0) !== '[' ||
+              attribute.charAt(attribute.length - 1) !== ']') {
+            isArray = false
+            break
+          }
+          let varSubscript = attribute.substring(1, attribute.length - 1)
+          if (!parseInt(varSubscript).toString() === varSubscript) {
+            isArray = false
+            break
+          }
+        }
+      }
+    }
+
+    // Merge arrays.
+    if (vars instanceof Array || isArray) {
+      let merged = []
+      varsIterator(function(attr) {
+        merged.push(attr)
+      })
+      diffIterator(function(varSubscript) {
+        let subscript =
+            parseInt(varSubscript.substring(1, varSubscript.length - 1))
+        let diffValue = diff[varSubscript]
+        while (subscript >= merged.length) {
+          merged.push(null)
+        }
+        merged[subscript] = Leanplum._mergeHelper(merged[subscript], diffValue)
+      })
+      return merged
+    }
+
+    // Merge dictionaries.
+    let merged = {}
+    varsIterator(function(attr) {
+      if (diff[attr] === null || diff[attr] === undefined) {
+        merged[attr] = vars[attr]
+      }
+    })
+    diffIterator(function(attr) {
+      merged[attr] = Leanplum._mergeHelper(vars !== null ? vars[attr] : null,
+          diff[attr])
+    })
+    return merged
+  }
+}
