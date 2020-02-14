89959b055cd867ab08cb4714a05ec37bc29223b1
Ignat Ignatov <ignat.ignatov@leanplum.com>
2020-02-14 10:24:55 +0200

    refactor: TypeScript declarations generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,28 @@
 const webpackConfig = require('./webpack.config')
+const libraryName = 'Leanplum'
 
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean')
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-eslint')
+  grunt.loadNpmTasks("grunt-ts")
   grunt.loadNpmTasks('grunt-webpack')
 
   grunt.initConfig({
-    clean: ['dist'],
+    clean: {
+      dist: ['dist'],
+      dts: ['dist/src']
+    },
     eslint: {
       target: ['lib/leanplum.js']
     },
-    webpack: {
-      options: {
-        stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
-      },
-      prod: webpackConfig,
-      dev: Object.assign({
-        watch: true
-      }, webpackConfig)
+    ts: {
+      dts: {
+        options: {
+          fast: 'never'
+        },
+        tsconfig: './tsconfig.dts.json'
+      }
     },
     uglify: {
       my_target: {
@@ -33,12 +37,48 @@ module.exports = function (grunt) {
         files: [
           'lib/leanplum.js'
         ],
-        tasks: ['lint', 'build']
+        tasks: ['lint', 'compile']
       }
+    },
+    webpack: {
+      options: {
+        stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+      },
+      dev: Object.assign({
+        watch: true
+      }, webpackConfig),
+      prod: webpackConfig
     }
   })
 
+  grunt.registerTask('bundle:dts', function () {
+    require('dts-bundle').bundle({
+      name: libraryName,
+      main: './dist/src/Leanplum.d.ts',
+      out: '../leanplum.d.ts',
+      removeSource: true,
+      headerText: `
+ *
+ *  Copyright 2020 Leanplum Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *`,
+      outputAsModuleFolder: true
+    })
+  })
+
   grunt.registerTask('lint', ['eslint'])
-  grunt.registerTask('build', ['clean', 'webpack:prod', 'uglify'])
+  grunt.registerTask('compile', ['clean:dist', 'webpack:prod'])
+  grunt.registerTask('build', ['clean:dist', 'ts:dts', 'bundle:dts', 'clean:dts', 'webpack:prod', 'uglify'])
   grunt.registerTask('default', ['webpack:dev'])
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,10 @@
 const path = require('path')
 
-const libraryName = 'Leanplum';
-
-function DtsBundlePlugin(){}
-DtsBundlePlugin.prototype.apply = function (compiler) {
-  compiler.plugin('done', function(){
-    var dts = require('dts-bundle');
-    dts.bundle({
-      name: libraryName,
-      main: './dist/src/Leanplum.d.ts',
-      out: '../leanplum.d.ts',
-      removeSource: true,
-      headerText: `
- *
- *  Copyright 2020 Leanplum Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *`,
-      outputAsModuleFolder: true
-    });
-  });
-};
+const libraryName = 'Leanplum'
 
 module.exports = {
   entry: {
-    leanplum: './src/bundles/leanplum.full.ts',
+    'leanplum': './src/bundles/leanplum.full.ts',
     'sw/sw': './src/PushServiceWorker.ts'
   },
   devtool: 'inline-source-map',
@@ -46,15 +16,12 @@ module.exports = {
     libraryTarget: 'umd'
   },
   resolve: {
-    extensions: [ '.ts' ],
+    extensions: ['.js', '.ts']
   },
-  plugins: [
-    new DtsBundlePlugin()
-  ],
   module: {
     rules: [{
       test: /\.ts$/,
-      exclude: /node_modules|test/,
+      include: [path.resolve(__dirname, './src')],
       loader: 'ts-loader'
     }]
   }
