1a5ade78e53c5ea0ada0df8100d612897fe10633
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-02-26 18:15:28 +0100

    test: add more varCache tests

--- test/specs/varcache.test.ts
+++ test/specs/varcache.test.ts
@@ -18,17 +18,42 @@ describe(VarCache, () => {
     expect(cache.getVariables()).toEqual({ foo: 2 })
   })
 
-  it('applyDiffs merges missing server variables', () => {
-    const cache = new VarCache()
+  describe('applyDiffs', () => {
+    it('updates variables', () => {
+      const cache = new VarCache()
 
-    cache.setVariables({ clientVar: 1 })
-    cache.applyDiffs({ serverVar: { nestedServerVar: 1 } }, null, null)
+      cache.setVariables({ foo: 1 })
+      cache.applyDiffs({ foo: 2 }, null, null)
 
-    expect(cache.getVariables()).toEqual({
-      clientVar: 1,
-      serverVar: {
-        nestedServerVar: 1
-      }
+      expect(cache.getVariables()).toEqual({ foo: 2 })
+    })
+
+    it('merges dictionaries', () => {
+      const cache = new VarCache()
+
+      cache.setVariables({ rootVar: { clientVar: 1 } })
+      cache.applyDiffs({ rootVar: { serverVar: 1 } }, null, null)
+
+      expect(cache.getVariables()).toEqual({
+        rootVar: {
+          clientVar: 1,
+          serverVar: 1
+        }
+      })
+    })
+
+    it('merges missing diff dictionaries', () => {
+      const cache = new VarCache()
+
+      cache.setVariables({ clientVar: 1 })
+      cache.applyDiffs({ serverVar: { nestedServerVar: 1 } }, null, null)
+
+      expect(cache.getVariables()).toEqual({
+        clientVar: 1,
+        serverVar: {
+          nestedServerVar: 1
+        }
+      })
     })
   })
 })
