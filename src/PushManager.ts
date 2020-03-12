0b843a4e5189a9551d9a38b74864ff9b77072d04
Ignat Ignatov <ignat.ignatov@leanplum.com>
2020-03-12 16:10:38 +0200

    chore: refactor `PushManager` class

--- src/PushManager.ts
+++ src/PushManager.ts
@@ -1,10 +1,9 @@
 /*
- *
  *  Copyright 2020 Leanplum Inc. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *  You may obtain a copy of the License at:
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,7 +11,7 @@
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
- *  limitations under the License
+ *  limitations under the License.
  *
  */
 
@@ -22,16 +21,18 @@ import Constants from './Constants'
 import LocalStorageManager from './LocalStorageManager'
 
 const APPLICATION_SERVER_PUBLIC_KEY =
-    'BInWPpWntfR39rgXSP04pqdmEdDGa50z6zqbMvxyxJCwzXIuSpSh8C888-CfJ82WELl7Xe8cjA' +
-    'nfCt-3vK0Ci68'
-
-let isSubscribed = false
-let serviceWorkerRegistration = null
+  'BInWPpWntfR39rgXSP04pqdmEdDGa50z6zqbMvxyxJCwzXIuSpSh8C888-CfJ82WELl7Xe8cjAnfCt-3vK0Ci68'
 
 /**
  * Push Manager handles the registration and subscription for web push.
  */
 export default class PushManager {
+  private serviceWorkerRegistration: ServiceWorkerRegistration | null = null
+
+  private get serviceWorker(): ServiceWorkerContainer {
+    return window.navigator.serviceWorker;
+  }
+
   public constructor(
     private createRequest: (action: string, args: ArgsBuilder, options: any) => void
   ) { }
@@ -41,169 +42,139 @@ export default class PushManager {
    * @return {Boolean} True if supported, else false.
    */
   public isWebPushSupported(): boolean {
-    return navigator && navigator.serviceWorker && 'serviceWorker' in navigator &&
-        'PushManager' in window
+    return Boolean(
+      window?.navigator !== undefined &&
+      'serviceWorker' in window.navigator &&
+      'PushManager' in window
+    )
   }
 
   /**
    * Whether or not the browser is subscribed to web push notifications.
    * @return {Promise} True if subscribed, else false.
    */
-  public isWebPushSubscribed(): Promise<boolean> {
-    if (!this.isWebPushSupported()) {
-      return new Promise((resolve) => {
-        resolve(false)
-      })
+  public async isWebPushSubscribed(): Promise<boolean> {
+    if (this.isWebPushSupported()) {
+      const registration = await this.getServiceWorkerRegistration()
+
+      if (registration) {
+        const subscription = await registration.pushManager.getSubscription()
+        const isSubscribed = subscription !== null
+
+        if (isSubscribed) {
+          this.updateNewSubscriptionOnServer(subscription)
+        }
+
+        return isSubscribed
+      }
     }
-    return this.getServiceWorkerRegistration()
-        .then((registration) => {
-          return new Promise((resolve) => {
-            if (!registration) {
-              resolve(false)
-            } else {
-              /** @namespace registration.pushManager The push manager object of the browser. **/
-              /** @namespace registration.pushManager.getSubscription **/
-              registration.pushManager.getSubscription()
-                  .then((subscription) => {
-                    isSubscribed = subscription !== null
-                    if (isSubscribed) {
-                      this.updateNewSubscriptionOnServer(subscription)
-                    }
-                    resolve(isSubscribed)
-                  })
-            }
-          })
-        })
+
+    return false
   }
 
   /**
    * Register for WebPush.
-   * @param  {String}   serviceWorkerUrl The url that serves the service worker
-   *                                     on your domain.
+   * @param  {String}   serviceWorkerUrl The url that serves the service worker on your domain.
    * @param  {Function} callback         The callback to be called with result.
    * @return {object} nothing
    */
-  public register(serviceWorkerUrl, callback): void {
+  public async register(
+    serviceWorkerUrl: string,
+    callback: (isSubscribed: boolean) => Promise<boolean>
+  ): Promise<boolean> {
     if (!this.isWebPushSupported()) {
       console.log('Leanplum: Push messaging is not supported.')
       return callback(false)
     }
-    navigator.serviceWorker.register(
-        serviceWorkerUrl ? serviceWorkerUrl : '/sw.min.js', null)
-        .then((registration) => {
-          serviceWorkerRegistration = registration
 
-          // Set the initial subscription value
-          serviceWorkerRegistration.pushManager.getSubscription()
-              .then((subscription) => {
-                isSubscribed = !(subscription === null)
-                if (isSubscribed) {
-                  this.updateNewSubscriptionOnServer(subscription)
-                }
-                if (callback) {
-                  return callback(isSubscribed)
-                }
-              })
-        })
-        .catch((error) => {
-          console.log('Leanplum: Service Worker Error: ', error)
-        })
+    try {
+      this.serviceWorkerRegistration = await this.serviceWorker.register(
+        serviceWorkerUrl || '/sw.min.js',
+        null
+      )
+
+      const subscription = this.serviceWorkerRegistration.pushManager.getSubscription()
+      const isSubscribed = subscription !== null
+
+      if (isSubscribed) {
+        this.updateNewSubscriptionOnServer(subscription)
+      }
+
+      return callback(isSubscribed)
+    } catch (error) {
+      console.log('Leanplum: Service Worker Error: ', error)
+      return callback(false)
+    }
   }
 
   /**
    * Subscribe the user(browser) to push.
    * @return {Promise} Resolves if subscription successful, otherwise rejects.
    */
-  public subscribeUser(): Promise<boolean> {
+  public async subscribeUser(): Promise<boolean> {
     const applicationServerKey = this.urlB64ToUint8Array(APPLICATION_SERVER_PUBLIC_KEY)
-    return new Promise((resolve, reject) => {
-      /** @namespace serviceWorkerRegistration.pushManager.subscribe Subscribe to push. **/
-      return serviceWorkerRegistration.pushManager.subscribe({
+
+    try {
+      const subscription = await this.serviceWorkerRegistration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey
       })
-          .then((subscription) => {
-            if (subscription) {
-              this.updateNewSubscriptionOnServer(subscription)
-              isSubscribed = true
-              return resolve(isSubscribed)
-            }
-            isSubscribed = false
-            return reject()
-          })
-          .catch((err) => {
-            return reject(`Leanplum: Failed to subscribe the user: ${err}`)
-          })
-    })
+
+      if (!subscription) {
+        throw new Error()
+      }
+
+      this.updateNewSubscriptionOnServer(subscription)
+    } catch (error) {
+      throw new Error(`Leanplum: Failed to subscribe the user: ${error}`)
+    }
+
+    return true
   }
 
   /**
    * Unsubscribe the user(browser) from push.
    * @return {Promise} Resolves if unsubscribed, otherwise rejects.
    */
-  public unsubscribeUser(): Promise<string> {
-    return new Promise((resolve, reject) => {
-      this.isWebPushSubscribed().then((subscribed) => {
-        if (!subscribed) {
-          return resolve()
+  public async unsubscribeUser(): Promise<void> {
+    let subscribed = await this.isWebPushSubscribed()
+
+    if (subscribed) {
+      try {
+        const subscription = await this.serviceWorkerRegistration.pushManager.getSubscription()
+
+        if (!subscription) {
+          throw new Error()
         }
 
-        serviceWorkerRegistration.pushManager.getSubscription()
-            .then((subscription) => {
-              if (subscription) {
-                /** @namespace serviceWorkerRegistration.pushManager.unsubscribe Unsubscribe to
-                 *  push. **/
-                return subscription.unsubscribe()
-              }
-              return reject()
-            })
-            .catch((error) => {
-              reject(`Leanplum: Error unsubscribing: ${error}`)
-            })
-            .then((success) => {
-              if (success) {
-                isSubscribed = false
-                return resolve()
-              }
-              return reject()
-            })
-      }, () => {
-        return reject()
-      })
-    })
+        await subscription.unsubscribe()
+      } catch (error) {
+        throw new Error(`Leanplum: Error unsubscribing: ${error}`)
+      }
+    }
   }
 
   /**
    * Retrieves the service worker registration object from browser.
    * @return {object} Returns the registration or null.
    */
-  private getServiceWorkerRegistration(): Promise<ServiceWorkerRegistration> {
-    return new Promise((resolve) => {
-      if (serviceWorkerRegistration) {
-        resolve(serviceWorkerRegistration)
-      } else {
-        /** @namespace navigator.serviceWorker.getRegistration Retrieves the  push registration
-         * from the browser. **/
-        navigator.serviceWorker.getRegistration().then((registration) => {
-          serviceWorkerRegistration = registration
-          resolve(registration)
-        })
-      }
-    })
+  private async getServiceWorkerRegistration(): Promise<ServiceWorkerRegistration> {
+    if (!this.serviceWorkerRegistration) {
+      this.serviceWorkerRegistration = await this.serviceWorker.getRegistration()
+    }
+
+    return this.serviceWorkerRegistration
   }
 
   /**
    * Encodes a base64 url string to an uint8 arrary.
-   * @param  {string} base64String [description]
-   * @return {Uint8Array}              [description]
+   * @param  {string} base64String
+   * @return {Uint8Array}
    */
   private urlB64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4)
-    const base64 = (base64String + padding)
-        .replace(/-/g, '+')
-        .replace(/_/g, '/')
-
-    const rawData = window.atob(base64)
+    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+    const rawData = this.decodeData(base64)
     const outputArray = new Uint8Array(rawData.length)
 
     for (let i = 0; i < rawData.length; ++i) {
@@ -224,10 +195,8 @@ export default class PushManager {
     let apply = Function.prototype.apply
     let key = subscription.getKey ? subscription.getKey('p256dh') : ''
     let auth = subscription.getKey ? subscription.getKey('auth') : ''
-    // noinspection ES6ModulesDependencies
-    let keyAscii = btoa(apply.call(String.fromCharCode, null, new Uint8Array(key)))
-    // noinspection ES6ModulesDependencies
-    let authAscii = btoa(apply.call(String.fromCharCode, null, new Uint8Array(auth)))
+    let keyAscii = this.encodeData(apply.call(String.fromCharCode, null, new Uint8Array(key)))
+    let authAscii = this.encodeData(apply.call(String.fromCharCode, null, new Uint8Array(auth)))
 
     return {
       endpoint: subscription.endpoint,
@@ -245,7 +214,8 @@ export default class PushManager {
       let preparedSubscription = this.prepareSubscription(subscription)
       let preparedSubscriptionString = JSON.stringify(preparedSubscription)
       let existingSubscriptionString = LocalStorageManager.getFromLocalStorage(
-          Constants.DEFAULT_KEYS.PUSH_SUBSCRIPTION)
+        Constants.DEFAULT_KEYS.PUSH_SUBSCRIPTION
+      )
 
       if (!isEqual(existingSubscriptionString, preparedSubscriptionString)) {
         LocalStorageManager.saveToLocalStorage(
@@ -262,15 +232,21 @@ export default class PushManager {
    * @param {String/Object} subscription The subscription string.
    */
   private setSubscription(subscription) {
-    if (!subscription) {
-      return
+    if (subscription) {
+      const args = new ArgsBuilder().add(Constants.PARAMS.WEB_PUSH_SUBSCRIPTION, subscription)
+
+      this.createRequest(Constants.METHODS.SET_DEVICE_ATTRIBUTES, args, {
+        queued: false,
+        sendNow: true
+      })
     }
+  }
 
-    const args = new ArgsBuilder().add(Constants.PARAMS.WEB_PUSH_SUBSCRIPTION, subscription)
+  private encodeData(data: string): string {
+    return window.btoa(data)
+  }
 
-    this.createRequest(Constants.METHODS.SET_DEVICE_ATTRIBUTES, args, {
-      queued: false,
-      sendNow: true
-    })
+  private decodeData(data: string): string {
+    return window.atob(data)
   }
 }
