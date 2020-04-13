72e610fc685885e0ce9cc7483519b82ade7c6210
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-04-13 18:00:09 +0300

    feat: finish rondo app

--- test/e2e/rondo.js
+++ test/e2e/rondo.js
@@ -1,30 +1,46 @@
 const isProdKey = (accessKey) => /^prod_/.test(accessKey);
-var refreshConfig = () => {
-    const appId = $('#appId').val();
-    const accessKey = $('#accessKey').val();
-    const apiPath = $('#apiPath').val();
+$("[data-action=setup]")
+    .submit((e) => {
+        e.preventDefault();
+        const appId = $('#appId').val();
+        const accessKey = $('#accessKey').val();
+        const apiPath = $('#apiPath').val();
 
-    if (appId && accessKey) {
-        if (isProdKey(accessKey)) {
-            Leanplum.setAppIdForProductionMode(appId, accessKey);
-        } else {
-            Leanplum.setAppIdForDevelopmentMode(appId, accessKey);
+        if (appId && accessKey) {
+            if (isProdKey(accessKey)) {
+                Leanplum.setAppIdForProductionMode(appId, accessKey);
+            } else {
+                Leanplum.setAppIdForDevelopmentMode(appId, accessKey);
+            }
         }
-    }
 
-    if (apiPath) {
-        Leanplum.setApiPath(apiPath);
-    }
-};
-$(".lp-config").on("change", refreshConfig);
-refreshConfig();
+        if (apiPath) {
+            Leanplum.setApiPath(apiPath);
+        }
+
+        $(".requires-setup").removeClass("requires-setup");
+    });
 
 $("#registerForWebPush")
     .click(() => Leanplum.registerForWebPush());
 
-// TODO: userId, userAttributes
+// TODO: userAttributes
 $("#start")
-    .click(() => Leanplum.start());
+    .click(() => {
+        const userId = $("startUserId").val();
+
+        const updateVars = () => {
+            const vars = Leanplum.getVariables();
+            const json = JSON.stringify(vars, null, 2);
+            $("#variables").text(json);
+        };
+
+        if (userId) {
+            Leanplum.start(userId, updateVars);
+        } else {
+            Leanplum.start(updateVars);
+        }
+    });
 
 $("#forceContentUpdate")
     .click(() => Leanplum.forceContentUpdate());
@@ -44,10 +60,25 @@ $("[data-action=track]")
         }
     });
 
-$("[data-action=state]")
+$("[data-action=advanceToState]")
     .submit(() => {
         const state = $("#stateName").val();
         Leanplum.advanceToState(state);
+    });
+
+$("[data-action=setUserId]")
+    .submit(() => {
+        const userId = $("#setUserId").val();
+        Leanplum.setUserId(userId);
+    });
+
+$("[data-action=setUserAttribute]")
+    .submit(() => {
+        const attribute = $("#attributeName").val();
+        const value = $("#attributeValue").val();
+        const attr = {};
+        attr[attribute] = value;
+        Leanplum.setUserAttributes(attr);
     });
 
 $("#sdkVersion").text(Leanplum.VERSION);
