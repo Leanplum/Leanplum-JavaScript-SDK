b8c5cb9c89e89cd1274274a9458132ed145e6632
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-04-25 01:35:43 +0300

    feat: app inbox class

---
+++ src/Inbox.ts
@@ -0,0 +1,104 @@
+import { CreateRequestFunction } from './types/internal'
+
+export default class LeanplumInbox {
+  private messageMap: { [key: string]: any };
+
+  constructor(
+    private createRequest: CreateRequestFunction
+  ) { }
+
+  public downloadMessages() {
+    this.createRequest('getNewsfeedMessages', undefined, {
+      // TODO: queued? sendNow?
+      response: (response) => {
+        if (!response.success) {
+          // TODO: error handling
+          return
+        }
+
+        this.messageMap = response.response[0].newsfeedMessages;
+      }
+    })
+
+  }
+
+  //TODO/////
+  public onChanged(handler: Function): void {}
+  public read(messageId: string): void {} // Promise?
+  public remove(messageId: string): void {} // Promise?
+  ///////////
+
+  public count(): number {
+    return Object.values(this.messageMap).length
+  }
+  public unreadCount(): number {
+    return Object.values(this.messageMap).filter(x => !x.isRead).length
+  }
+  private messages(filter: Function) {
+    const result = []
+    for (let id in this.messageMap) {
+      const data = this.messageMap[id]
+      if (filter(data)) {
+        result.push(InboxMessage.create(id, data))
+      }
+    }
+    return result
+  }
+  public allMessages(): InboxMessage[] {
+    return this.messages(() => true)
+  }
+  public unreadMessages(): InboxMessage[] {
+    return this.messages((x) => !x.isRead)
+  }
+  public messageIds(): string[] {
+    return Object.keys(this.messageMap)
+  }
+  public message(id: string): InboxMessage {
+    const message = this.messageMap[id]
+    if (!message) {
+      return null
+    }
+    return InboxMessage.create(id, message)
+  }
+}
+
+class InboxMessage {
+  static create(id: string, messageInfo: any): InboxMessage {
+    return new InboxMessage(
+      id,
+      messageInfo.messageData?.vars?.Title,
+      messageInfo.messageData?.vars?.Subtitle,
+      messageInfo.deliveryTimestamp,
+      messageInfo.isRead,
+      messageInfo.messageData?.vars?.Image
+    )
+  }
+
+  constructor(
+    private _id: string,
+    private _title: string,
+    private _subtitle: string,
+    private _timestamp: number,
+    private _isRead: boolean,
+    private _imageUrl: string
+  ) { }
+
+  public id(): string {
+    return this._id
+  }
+  public title(): string {
+    return this._title
+  }
+  public subtitle(): string {
+    return this._subtitle
+  }
+  public timestamp(): number {
+    return this._timestamp
+  }
+  public isRead(): boolean {
+    return this._isRead
+  }
+  public imageUrl(): string {
+    return this._imageUrl
+  }
+}
---
+++ test/specs/Inbox.test.ts
@@ -0,0 +1,160 @@
+import Inbox from '../../src/Inbox'
+
+describe(Inbox, () => {
+  let inbox: Inbox
+  const createRequestSpy: jest.Mock<void> = jest.fn()
+
+  beforeEach(() => inbox = new Inbox(createRequestSpy))
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('downloadMessages', () => {
+    it('fetches messages from API', () => {
+      inbox.downloadMessages()
+
+      expect(createRequestSpy).toHaveBeenCalledTimes(1)
+      expect(createRequestSpy.mock.calls[0][0]).toEqual('getNewsfeedMessages')
+    })
+  })
+
+  describe('allMessages', () => {
+    it('returns message objects', () => {
+      mockMessages({
+        '123##1': createMessage({
+          deliveryTimestamp: 1585569600625,
+          isRead: true,
+          vars: {
+            Title: "Message title",
+            Subtitle: "Message subtitle",
+            Image: "https://example.com/image.png",
+          }
+        })
+      })
+
+      const messages = inbox.allMessages()
+
+      expect(messages.length).toEqual(1)
+      expect(messages[0].id()).toEqual('123##1')
+      expect(messages[0].title()).toEqual('Message title')
+      expect(messages[0].subtitle()).toEqual('Message subtitle')
+      expect(messages[0].imageUrl()).toEqual('https://example.com/image.png')
+      expect(messages[0].isRead()).toEqual(true)
+      expect(messages[0].timestamp()).toEqual(1585569600625)
+    })
+  })
+
+  describe('messageIds', () => {
+    it('returns all message IDs', () => {
+      mockMessages({
+        '123##1': {},
+        '234##1': {},
+        '345##1': {}
+      })
+
+      expect(inbox.messageIds()).toEqual([
+        '123##1',
+        '234##1',
+        '345##1'
+      ])
+    })
+  })
+
+  describe('unreadMessages', () => {
+    it('filters out read messages', () => {
+      mockMessages({
+        '123##1': { isRead: true },
+        '234##1': { isRead: false },
+        '345##1': { isRead: false }
+      })
+
+      const messages = inbox.unreadMessages()
+      expect(messages.length).toEqual(2)
+      expect(messages.map(x => x.id()).sort()).toEqual([
+        '234##1',
+        '345##1'
+      ])
+    })
+  })
+
+  describe('count', () => {
+    it('returns message count', () => {
+      mockMessages({
+        '123##1': { isRead: true },
+        '234##1': { isRead: false },
+        '345##1': { isRead: false }
+      })
+
+      expect(inbox.count()).toEqual(3)
+    })
+  })
+
+  describe('unreadCount', () => {
+    it('returns unread message count', () => {
+      mockMessages({
+        '123##1': { isRead: true },
+        '234##1': { isRead: false },
+        '345##1': { isRead: false }
+      })
+
+      expect(inbox.unreadCount()).toEqual(2)
+    })
+  })
+
+  describe('message', () => {
+    it('returns message object for given id', () => {
+      mockMessages({
+        '123##1': createMessage({
+          vars: {
+            Title: 'Target'
+          }
+        }),
+        '234##1': { isRead: false },
+        '345##1': { isRead: false }
+      })
+
+      expect(inbox.message('123##1').title()).toEqual('Target')
+    })
+
+    it('returns null for unknown message', () => {
+      mockMessages({})
+
+      expect(inbox.message('123##1')).toEqual(null)
+    })
+  })
+
+  function mockMessages(newsfeedMessages: any) {
+    createRequestSpy.mockImplementationOnce(
+      (method, args, options) => {
+        options.response({
+          success: true,
+          response: [ { newsfeedMessages } ]
+        })
+      }
+    )
+    inbox.downloadMessages()
+  }
+
+  function createMessage(options: any) {
+    return {
+      deliveryTimestamp: options.deliveryTimestamp || Date.now(),
+      isRead: options.isRead || false,
+      messageData: {
+        countdown: 1,
+        action: "__Newsfeed Message",
+        startTime: 1581339600000,
+        parentCampaignId: 54321,
+        vars: Object.assign({
+          __name__:"__Newsfeed Message",
+          Title: "Message title",
+          Subtitle: "Message subtitle",
+          Image: "https://example.com/image.png",
+          "Open action": {"__name__":""}
+        }, options.vars),
+        hasImpressionCriteria: false,
+        priority: 1000
+      }
+    }
+  }
+})
