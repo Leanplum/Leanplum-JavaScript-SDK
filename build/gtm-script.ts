656454aafb2e2a4e029355a3e2fb3f50af117386
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-05-20 18:58:30 +0300

    chore: build script for GTM repo

--- build/gtm-script.ts
+++ build/gtm-script.ts
@@ -2,35 +2,76 @@ import { exec } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 import Leanplum from '../src/LeanplumInternal';
 
+const SUBMODULE_PATH = './gtm-tag';
+
+const readText = (filename: string): string =>
+  readFileSync(filename, { encoding: 'utf-8' });
+
+const promiseExecution = (command: string): Promise<any> =>
+  new Promise((resolve, reject) =>
+    exec(command, { encoding: 'utf-8' }, (err: any, out: any) =>
+      err ? reject(err.message) : resolve(out.trim())));
+
+const execParallel = (commands: string[]): Promise<any[]> =>
+  Promise.all(commands.map(promiseExecution))
+
+const execSequential = (commands: string[]): Promise<any> =>
+  commands.reduce((promise: Promise<any>, command: string) =>
+    promise.then(() => promiseExecution(command)),
+    Promise.resolve()
+  )
+
 async function run() {
   const allMethods = Object.getOwnPropertyNames(Leanplum.prototype);
   // hack: createRequest is the first private method, thus slicing all public ones, removing the constructor (first field)
   const publicMethods = allMethods.slice(1, allMethods.indexOf('createRequest'));
-
-  const execAll = (commands: string[]): Promise<any[]> =>
-    Promise.all(
-      commands.map((command: string) =>
-        new Promise((resolve, reject) =>
-          exec(command, { encoding: 'utf-8' }, (err: any, out: any) =>
-            err ? reject(err.message) : resolve(out.trim())))));
-
-  const [ version, sha ] = await execAll([
+  let [ version ] = await execParallel([
     'npm show leanplum-sdk version',
-    'git rev-parse HEAD'
-    // git submodule update --init
+    'git submodule update --init'
   ]);
 
-  // TODO: fetch submodule repo
+  if (!(/^\d+\.\d+\.\d+$/).test(version)) {
+    throw new Error(`Could not parse version: ${version}`);
+  }
 
+  const releaseInfo = await execSequential([
+    `curl https://api.github.com/repos/Leanplum/Leanplum-JavaScript-SDK/releases/tags/${version}`
+  ]);
+  const releaseNotes =
+    JSON.parse(releaseInfo).body
+      .split(/\r?\n/)
+      .filter(line => !!line.trim())
+      .join('\n     ');
 
-  const template = readFileSync('./build/gtm-template.tpl', { encoding: 'utf-8' })
+  const template = readText('./build/gtm-template.tpl')
     .replace(/{LP_SDK_VERSION}/g, version)
     .replace(/{LP_SDK_METHODS}/g, JSON.stringify(publicMethods));
+  writeFileSync(`${SUBMODULE_PATH}/template.tpl`, template);
 
-  writeFileSync('./gtm-lp-tag/template.tpl', template);
+  const sha = await execSequential([
+    `git -C ${SUBMODULE_PATH} checkout master`,
+    `git -C ${SUBMODULE_PATH} add .`,
+    `git -C ${SUBMODULE_PATH} commit -m "chore: update SDK to ${version}"`,
+    `git -C ${SUBMODULE_PATH} rev-parse HEAD`
+  ]);
 
-  // TODO: update metadata with sha
-  console.log(sha);
+  let metadata = readText(`${SUBMODULE_PATH}/metadata.yaml`);
+  if (metadata.indexOf(sha) < 0) {
+    const lines = metadata.split('\n');
+    lines.splice(
+      lines.indexOf('versions:') + 1,
+      0,
+      ` - sha: ${sha}`,
+      `   changeNotes: |2\n     ${releaseNotes}`
+    );
+    metadata = lines.join('\n');
+  }
+  writeFileSync(`${SUBMODULE_PATH}/metadata.yaml`, metadata);
+
+  await execSequential([
+    `git -C ${SUBMODULE_PATH} add .`,
+    `git -C ${SUBMODULE_PATH} commit -m "chore: publish new version"`,
+  ])
 
   // TODO: push submodule
 }
