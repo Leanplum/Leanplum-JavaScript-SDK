ceab75e7e6bbe8e5863902e7c19cc462d9f12c16
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-06-12 12:20:39 +0300

    feat: add message previews for in-app messages

--- test/specs/Messages.test.ts
+++ test/specs/Messages.test.ts
@@ -691,4 +691,63 @@ describe(Messages, () => {
       expect(showMessage).toHaveBeenCalledTimes(0)
     })
   })
+
+  describe('message preview', () => {
+    it('triggers a "showMessage" after receiving a preview request', () => {
+      events.emit('previewRequest', MESSAGE)
+
+      expect(showMessage).toHaveBeenCalledTimes(1)
+
+      expect(showMessage).toHaveBeenCalledWith({
+        isPreview: true,
+        message: {
+          messageId: MESSAGE.messageId,
+          ...MESSAGE.action
+        },
+        context: {
+          track: expect.any(Function),
+          runActionNamed: expect.any(Function),
+          runTrackedActionNamed: expect.any(Function)
+        }
+      })
+    })
+
+    it('resolves file references', () => {
+      const fileName = 'lp_public_sf_ui_font.css'
+      const fileUrl = 'https://example.com/styles.css'
+
+      events.emit('filesReceived', {
+        [fileName]: { '': { servingUrl: fileUrl } }
+      })
+
+      events.emit('previewRequest', {
+        messageId: 123,
+        action: {
+          '__file__CSS File': fileName
+        }
+      })
+
+      expect(showMessage.mock.calls[0][0].message).toEqual(
+        expect.objectContaining({
+          'CSS File URL': fileUrl
+        })
+      )
+    })
+
+    it('resolves colors to hex', () => {
+      events.emit('previewRequest', {
+        messageId: 123,
+        action: {
+          'Red Color': -65536,
+          'Green Color': -16711936,
+          'White Color': -1
+        }
+      })
+
+      const message = showMessage.mock.calls[0][0].message
+      expect(message).toHaveProperty('Red Color', 'rgba(255,0,0,1)')
+      expect(message).toHaveProperty('Green Color', 'rgba(0,255,0,1)')
+      expect(message).toHaveProperty('White Color', 'rgba(255,255,255,1)')
+    })
+  })
 })
