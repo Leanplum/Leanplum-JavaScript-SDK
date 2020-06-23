7874b8f727187fb44084e7c79b861aaf1facb256
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-06-24 00:00:07 +0300

    chore: handle more floating intersititial cases (#109)
    * fix: always stretch hero image
    
    * fix: show interstitial buttons by default
    
    * style: apply PR suggestion

--- test/e2e/index.ts
+++ test/e2e/index.ts
@@ -251,7 +251,7 @@ Leanplum.on('showMessage', (args) => {
     const imageInfo = message['Hero image']
     const imageUrl = imageInfo?.['Image URL']
     if (imageUrl) {
-      const imageHtml = `<p><img src="${imageUrl}" width="${imageInfo.width || '100%'}" height="${imageInfo.height || ''}" /></p>`
+      const imageHtml = `<p><img src="${imageUrl}" width="100%" /></p>`
       if (!imageInfo['Display above headline']) {
         body += imageHtml;
       } else {
@@ -265,7 +265,7 @@ Leanplum.on('showMessage', (args) => {
         console.log(`Could not find ${buttonName} in message: `, message);
         return
       }
-      if (!button['Show button']) {
+      if (button['Show button'] === false) {
         return
       }
       buttons.push({
