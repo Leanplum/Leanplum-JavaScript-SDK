b6e32f5104ed8e7483ccc5d258ce19ac774f4cf4
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2020-06-23 20:48:44 +0300

    fix: always stretch hero image

--- test/e2e/index.ts
+++ test/e2e/index.ts
@@ -251,7 +251,7 @@ Leanplum.on('showMessage', (args) => {
     const imageInfo = message['Hero image']
     const imageUrl = imageInfo?.['Image URL']
     if (imageUrl) {
-      const imageHtml = `<p><img src="${imageUrl}" width="${imageInfo.width || '100%'}" height="${imageInfo.height || ''}" /></p>`
+      const imageHtml = `<p><img src="${imageUrl}" width="100%" /></p>`
       if (!imageInfo['Display above headline']) {
         body += imageHtml;
       } else {
